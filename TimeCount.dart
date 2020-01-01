8e064453d5c101e18f890136c9ec52b6c4de8a57
Aero <bash.uid0@gmail.com>
2020-01-01 14:31:46 +0800

    重构拆分计时器和动画减少渲染范围。

---
+++ TimeCount.dart
@@ -0,0 +1,187 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class TimeCount extends StatefulWidget {
+  TimeCount({Key key}) : super(key: key);
+
+  @override
+  TimeCountState createState() => TimeCountState();
+}
+
+class TimeCountState extends State<TimeCount> {
+  DateTime lastPopTime;
+  Timer _timer;
+  int _count;
+  bool _relaxTimeState = false;
+  bool _visible = false;
+  bool _pause = false;
+  int _workTimeCount = 3;
+  int _relaxTimeCount = 3;
+
+  String _note = "长按结束";
+
+  //计时器
+  void _timerCount() {
+    //休息状态
+    if (_relaxTimeState) {
+      _timer = Timer.periodic(
+        Duration(seconds: 1),
+        (Timer timer) {
+          if (_count == 0) {
+            _timer.cancel();
+            //重置为工作状态
+            resetToWorkTime();
+            //发送消息
+            TimeCountNotification(signal: false).dispatch(context);
+            return;
+          }
+          setState(() {
+            _count--;
+          });
+        },
+      );
+      //工作状态
+    } else if (!_relaxTimeState) {
+      _timer = Timer.periodic(
+        Duration(seconds: 1),
+        (Timer timer) {
+          if (_count == 0) {
+            _timer.cancel();
+            //进入休息状态
+            relaxTime();
+            //发送消息
+            TimeCountNotification(signal: true).dispatch(context);
+            return;
+          }
+          setState(() {
+            _count--;
+          });
+        },
+      );
+      return;
+    }
+  }
+
+  //分秒换算
+  String _transSecond() {
+    int minute = _count ~/ 60;
+    int seconds = _count % 60;
+    if (_count % 60 == 0) {
+      return ("$minute:$seconds\0");
+    } else if (_count % 60 <= 9 && _count > 0) {
+      return ("$minute:0$seconds");
+    }
+    return ("$minute:$seconds");
+  }
+
+  //启动状态设置
+  void startTime() {
+    setState(() {
+      _visible = true;
+    });
+    _timerCount();
+  }
+
+  //休息状态设置
+  void relaxTime() {
+    setState(() {
+      _relaxTimeState = true;
+      _count = _relaxTimeCount;
+      _note = "长按跳过";
+    });
+    _timerCount();
+  }
+
+  //重设为工作状态
+  void resetToWorkTime() {
+    _count = _workTimeCount;
+    _relaxTimeState = false;
+    _note = "长按结束";
+    _pause = false;
+    _timerCount();
+  }
+
+  //取消状态设置
+  void cancelTime() {
+    if (_relaxTimeState) {
+      _timer.cancel();
+      resetToWorkTime();
+      return;
+    }
+    _timer.cancel();
+    setState(() {
+      _visible = false;
+      _relaxTimeState = false;
+      _pause = false;
+      _count = _workTimeCount;
+    });
+  }
+
+  //暂停
+  void _pauseTime() {
+    if (!_pause) {
+      _timer.cancel();
+      setState(() {
+        _pause = true;
+      });
+      return;
+    }
+    _timerCount();
+    setState(() {
+      _pause = false;
+    });
+  }
+
+  @override
+  void initState() {
+    _count = _workTimeCount;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          AnimatedPadding(
+            padding: EdgeInsets.only(top: 105),
+            duration: Duration(milliseconds: 500),
+            child: Text(
+              _transSecond(),
+              style: TextStyle(color: Colors.white, fontSize: 35),
+            ),
+          ),
+          Visibility(
+            visible: _visible,
+            child: CupertinoButton(
+              child: Icon(
+                _pause ? Icons.play_arrow : Icons.pause,
+                color: Colors.white,
+                size: 30,
+              ),
+              onPressed: () {
+                _pauseTime();
+              },
+            ),
+          ),
+          Visibility(
+            visible: _visible,
+            child: Text(
+              _note,
+              style: TextStyle(color: Colors.white, fontSize: 10),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TimeCountNotification extends Notification {
+  TimeCountNotification({this.signal});
+
+  final bool signal;
+}
--- main.dart
+++ main.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:uptime/TimeCount.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,239 +23,128 @@ class AppHome extends StatefulWidget {
 
 class _MyAppState extends State<AppHome> {
   final double _top = 50;
-  Timer _timer;
-  int _count;
-  bool _relaxTimeState = false;
-  bool _visible = false;
-  bool _play = false;
-  bool _pause = false;
-  int _workTimeCount = 1500;
-  int _relaxTimeCount = 300;
   double _boxHeight = 250;
   double _boxWidth = 250;
-  String _note = "长按结束";
   Color _color1 = Colors.orange[300];
   Color _color2 = Colors.deepOrange;
   Color _shadowC = Colors.deepOrange[500];
+  bool _play = false;
+  bool _relaxTime = false;
+  static GlobalKey<TimeCountState> _globalKey = GlobalKey();
 
-  //计时器
-  void _timerCount() {
-    if (!_relaxTimeState) {
-      _timer = Timer.periodic(
-        Duration(seconds: 1),
-        (Timer timer) {
-          if (_count == 0) {
-            _timer.cancel();
-            _relaxTime();
-            return;
-          }
-          setState(() {
-            _count--;
-          });
-        },
-      );
-      return;
-    } else if (_relaxTimeState) {
-      _timer = Timer.periodic(
-        Duration(seconds: 1),
-        (Timer timer) {
-          if (_count == 0) {
-            _resetRelaxTime();
-            _startTime();
-            return;
-          }
-          setState(() {
-            _count--;
-          });
-        },
-      );
-    }
-  }
-
-  //分秒换算
-  String _transSecond() {
-    int minute = _count ~/ 60;
-    int seconds = _count % 60;
-    if (_count % 60 == 0) {
-      return ("$minute:$seconds\0");
-    } else if (_count % 60 <= 9 && _count > 0) {
-      return ("$minute:0$seconds");
-    }
-    return ("$minute:$seconds");
-  }
-
-  //启动状态设置
-  void _startTime() {
-    Future.delayed(Duration(milliseconds: 1), () {
-      _timerCount();
-    });
+  //启动动画
+  void _startAnimated() {
     setState(() {
-      _count = _workTimeCount;
-      _visible = true;
-      _play = true;
-      _boxWidth = 330;
-      _boxHeight = 330;
       _color1 = Colors.orange;
       _color2 = Colors.red;
       _shadowC = Colors.deepOrange[500];
+      _boxWidth = 330;
+      _boxHeight = 330;
     });
   }
 
-  //休息状态设置
-  void _relaxTime() {
+  void _relaxAnimated() {
     setState(() {
       _color1 = Colors.lightGreenAccent;
       _color2 = Colors.green;
       _shadowC = Colors.green;
-      _relaxTimeState = true;
-      _count = _relaxTimeCount;
-      _note = "长按跳过";
     });
-    _timerCount();
   }
 
-  //重设休息后状态
-  void _resetRelaxTime() {
-    _timer.cancel();
-    _relaxTimeState = false;
-    _note = "长按结束";
-  }
-
-  //取消状态设置
-  void _cancelTime() {
-    if (_relaxTimeState) {
-      _resetRelaxTime();
-      _startTime();
-      return;
-    }
-    _timer.cancel();
-    _visible = false;
-    _relaxTimeState = false;
-    setState(() {
-      _count = _workTimeCount;
-      _play = false;
-      _color1 = Colors.orange[300];
-      _color2 = Colors.deepOrange;
-      _shadowC = Colors.deepOrange[500];
-    });
-    _boxWidth = 250;
-    _boxHeight = 250;
-  }
-
-  //暂停
-  void _pauseTime() {
-    if (!_pause) {
-      _timer.cancel();
+  void _cancelAnimated() {
+    if (_relaxTime) {
       setState(() {
-        _pause = true;
+        _color1 = Colors.orange[300];
+        _color2 = Colors.deepOrange;
+        _shadowC = Colors.deepOrange[500];
+        _relaxTime = false;
       });
       return;
     }
-    _timerCount();
     setState(() {
-      _pause = false;
+      _color1 = Colors.orange[300];
+      _color2 = Colors.deepOrange;
+      _shadowC = Colors.deepOrange[500];
+      _boxWidth = 250;
+      _boxHeight = 250;
     });
-  }
-
-  @override
-  void initState() {
-    _count = _workTimeCount;
-    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Container(
-        color: Colors.white,
-        child: Padding(
-          padding: EdgeInsets.only(top: _top),
-          child: Stack(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 20, top: 5),
-                child: Text(
-                  "番茄钟",
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
+    return NotificationListener<TimeCountNotification>(
+      onNotification: (onNotification) {
+        if (onNotification.signal) {
+          _relaxAnimated();
+          _relaxTime = true;
+          return false;
+        }
+        _startAnimated();
+        _relaxTime = false;
+        return false;
+      },
+      child: CupertinoPageScaffold(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.only(top: _top),
+            child: Stack(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(left: 20, top: 5),
+                  child: Text(
+                    "番茄钟",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Center(
-                child: GestureDetector(
-                  //判断是否计时状态
-                  onTap: () {
-                    if (!_play) {
-                      //开始计时
-                      _startTime();
-                    }
-                  },
-                  onLongPress: () {
-                    //取消计时
-                    _cancelTime();
-                  },
-                  child: AnimatedContainer(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(300)),
-                      gradient: RadialGradient(
-                        colors: [_color1, _color2],
-                        center: Alignment.topLeft,
-                        radius: 1.3,
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: _shadowC,
-                          blurRadius: 12,
-                          offset: Offset(2, 5),
-                        )
-                      ],
-                    ),
-                    duration: Duration(milliseconds: 1000),
-                    curve: Curves.elasticOut,
-                    height: _boxHeight,
-                    width: _boxWidth,
-                    child: Center(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          AnimatedPadding(
-                            padding: EdgeInsets.only(top: 105),
-                            duration: Duration(milliseconds: 500),
-                            child: Text(
-                              _transSecond(),
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 35),
-                            ),
-                          ),
-                          Visibility(
-                            visible: _visible,
-                            child: CupertinoButton(
-                              child: Icon(
-                                _pause ? Icons.play_arrow : Icons.pause,
-                                color: Colors.white,
-                                size: 30,
-                              ),
-                              onPressed: () {
-                                _pauseTime();
-                              },
-                            ),
-                          ),
-                          Visibility(
-                            visible: _visible,
-                            child: Text(
-                              _note,
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 10),
-                            ),
+                Center(
+                  child: GestureDetector(
+                    //判断是否计时状态
+                    onTap: () {
+                      if (!_play) {
+                        //开始计时
+                        _globalKey.currentState.startTime();
+                        _startAnimated();
+                        _play = true;
+                      }
+                    },
+                    onLongPress: () {
+                      //取消计时
+                      _globalKey.currentState.cancelTime();
+                      _cancelAnimated();
+                      _play = false;
+                    },
+                    child: AnimatedContainer(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(300)),
+                        gradient: RadialGradient(
+                          colors: [_color1, _color2],
+                          center: Alignment.topLeft,
+                          radius: 1.3,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            color: _shadowC,
+                            blurRadius: 12,
+                            offset: Offset(2, 5),
                           )
                         ],
                       ),
+                      duration: Duration(milliseconds: 1000),
+                      curve: Curves.elasticOut,
+                      height: _boxHeight,
+                      width: _boxWidth,
+                      child: TimeCount(
+                        key: _globalKey,
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
